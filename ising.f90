81e83e6eaa747a8422be38317032887b178bf471
Callum Hughes <callumhughes2000@live.co.uk>
2022-02-08 18:59:54 +0000

    Added subroutines to calculate total lattice energy.

--- ising.f90
+++ ising.f90
@@ -7,15 +7,15 @@ program ising
 !********************************************************************
   !!! Defines constants !!!
   !real (kind=dp), parameter :: kB = 1
-  real (kind=dp), parameter :: h = 0.01
+  !real (kind=dp), parameter :: h = 0.01
 
   !!! Defines arrays !!!
   real (kind=dp), dimension(0:4,2) :: probArray
   integer, dimension(:,:), allocatable :: isingGrid
-  real (kind=dp), dimension(:), allocatable :: magData
+  real (kind=dp), dimension(:), allocatable :: magData, energyData
 
   !!! Defines variables
-  real (kind=dp) ::  probValue
+  real (kind=dp) ::  probValue, magAvg, energyAvg, totalLatticeEnergy
   integer :: isingWidth, isingHeight, i, j, timestep, spinSi, downSpins, numTimeSteps, temp
   integer :: neighbour1, neighbour2, neighbour3, neighbour4, neighbourSum, sumSi
   logical :: flipAllowed
@@ -24,9 +24,10 @@ program ising
 !************************** MAIN PROGRAMME **************************
 !********************************************************************
 
-  numTimeSteps = 500
+  numTimeSteps = 1000
   allocate(magData(numTimeSteps))
-  temp = 3
+  allocate(energyData(numTimeSteps))
+  temp = 1
 
   ! Precalculate the probability array
   spinSi = 1
@@ -39,23 +40,37 @@ program ising
   !isingGrid(1,5) = -1
   !isingGrid(5,3) = -1
   !isingGrid(3,4) = -1
-  print *, isingGrid
+  print *, 'initial ising: ', isingGrid
+
+  timestep=0
+  sumSi=0
+  call SumLatticeSpins
+  print *, sumSi
+  call AssignMagData
 
   do timestep = 1, numTimeSteps
     sumSi = 0 ! Resets the sum of the spins of lattice points of the system
+    !call SumLatticeSpins
+    !print *, 'sum is: ', sumSi
+    !call AssignMagData
+    !call AssignEnergyData
+    totalLatticeEnergy = 0
     do j = 1, isingHeight
       do i = 1, isingWidth
+        !print *, Magnetisation(sumSi,isingWidth,isingHeight)
         call CheckSpin
         call SumNeighbourSpin
         call CheckNeighbourSum
         call FindProbability
         call FlipSpin
         call UpdateIsingGrid
-        call SumLatticeSpins
+        !call StateEnergy
         !print *, ' ' ! Creates gap between output statements for each step
       end do
     end do
+    call SumLatticeSpins
     call AssignMagData
+    !print *, 'total lattice energy is: ', totalLatticeEnergy
   end do
 
   print *, isingGrid
@@ -63,6 +78,14 @@ program ising
   !print *, magData
 
   print *, probArray
+
+  call MagDataAvg
+
+  print *, 'Mag Avg is: ', magAvg
+
+  !call EnergyDataAvg
+
+  !print *, 'Energy Avg is: ', energyAvg
 
 !********************************************************************
 !******************** FUNCTIONS AND SUBROUTINES *********************
@@ -73,7 +96,7 @@ contains
     real (kind=dp) :: Energy
     integer :: Si, neighbours
     real (kind=dp), parameter :: J = 1.0
-    real (kind=dp), parameter :: h = 0.0
+    real (kind=dp), parameter :: h = -0.01
 
     Energy = -(J*Si*neighbours)-(h*Si)
   end function Energy
@@ -86,12 +109,12 @@ contains
     Probability = exp(-(Energy(Si*(-1),neighbours)-Energy(Si,neighbours))/temp)
   end function
 
-  function Magnetisation(sumSi)
+  function Magnetisation(sumSi,width,height)
     !!! Function to calculate the Magnetisation of the system !!!
     real (kind=dp) :: Magnetisation
-    integer :: sumSi
+    integer :: sumSi, width, height
 
-    Magnetisation = (1.0/(isingWidth*isingHeight))*(sumSi)
+    Magnetisation = (1.0/(width*height))*(sumSi)
   end function
 
   subroutine CreateIsingGrid
@@ -224,6 +247,8 @@ contains
 
     call Random_Number(r)
 
+    print *, 'Random number is: ', r
+
     if (r.LT.probValue) then
       spinSi = spinSi * (-1)
       flipAllowed = .True.
@@ -250,16 +275,64 @@ contains
 
   subroutine SumLatticeSpins
     !!! Subroutine to sum the spins of all the lattice points in the system
-    sumSi = sumSi + isingGrid(i,j)
+    do j = 1, isingHeight
+      do i = 1, isingWidth
+        sumSi = sumSi + isingGrid(i,j)
+        !print *, 'current spin is: ', isingGrid(i,j)
+        !print *, 'sum is: ', sumSi
+      end do
+    end do
   end subroutine
 
   subroutine AssignMagData
     !!! Assigns values of Magnetisation to magData array for each time step !!!
-    magData(timestep) = Magnetisation(sumSi)
+    !!! Writes magnetisation values out to a data file alongside the current timestep !!!
+    magData(timestep) = Magnetisation(sumSi,isingWidth,isingHeight)
 
     open(unit=1, file='magdata.dat')
 
     write(1,*) timestep, magData(timestep)
+  end subroutine
+
+  subroutine MagDataAvg
+    !!! Subroutine to calculate the average of Magnetisation data !!!
+    integer :: k
+    real (kind=dp) :: magTot
+
+    do k = 1, size(magData)
+      magTot = magTot + magData(k)
+    end do
+
+    magAvg = magTot / size(magData)
+  end subroutine
+
+  subroutine AssignEnergyData
+    !!! Calculates Energy of each lattice point and sums them to find Total Lattice Energy, then assigns values to energyData array for each time step !!!
+    !!! Writes energy values alongside current timestep out to a data file !!!
+    energyData(timestep) = totalLatticeEnergy
+
+    print *, 'Energy is: ', energyData(timestep)
+
+    open(unit=2, file='energydata.dat')
+
+    write(2,*) timestep, energyData(timestep)
+  end subroutine
+
+  subroutine StateEnergy
+    !!! Calculates energy of state at current lattice point !!!
+    totalLatticeEnergy = totalLatticeEnergy + Energy(spinSi,neighbourSum)
+  end subroutine
+
+  subroutine EnergyDataAvg
+    !!! Calculates the average of the energy data !!!
+    integer :: k
+    real (kind=dp) :: energyTot
+
+    do k = 1, size(energyData)
+      energyTot = energyTot + energyData(k)
+    end do
+
+    energyAvg = energyTot / size(energyData)
   end subroutine
 
 end program ising
@@ -275,3 +348,4 @@ end program ising
 ! – Add subroutine for garbage collection
 ! – Add graph for Magnetisation vs Temp and include reference
 ! – Calculating total lattice energy???
+! – If add modules, functions and subroutines start with module name
